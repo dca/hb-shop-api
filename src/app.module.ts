069b8ff1efa5719f190faacf62fdca5e931c8cf3
Dca <dcahsu@gmail.com>
2021-08-10 12:12:38 +0800

    feat(product): Create product resource by nest-cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmService } from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmService,
     }),
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
